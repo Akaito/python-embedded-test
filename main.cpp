cfa7f2cdcf9026130fef556d9d77a4a808b8289e
Chris Barrett <chris@codesaru.com>
2017-10-09 10:03:50 -0700

    Re-organize to make cleaner room for later tests.

--- main.cpp
+++ main.cpp
@@ -2,20 +2,24 @@
 
 #include <Python.h>
 
+
+void runfile(const char * filename) {
+	FILE * pyfile = std::fopen(filename, "rt");
+	PyRun_SimpleFileEx(
+		pyfile,
+		filename, // for debugging
+		true // close the file for me
+	);
+}
+
+
 int main (int argc, const char ** argv) {
 	Py_Initialize();
 
 	// test executing Python passed from program
 	PyRun_SimpleString("print('test {}'.format(2 + 8))");
 
-	// run a .py file
-	FILE * pyfile = std::fopen("test.py", "rt");
-	PyRun_SimpleFileEx(
-		pyfile,
-		"test.py", // for debugging
-		true // close the file for me
-	);
-	pyfile = nullptr;
+	runfile("test.py");
 
 	return 0;
 }
