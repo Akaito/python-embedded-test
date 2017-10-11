65196359d1342e88019939c21c67ae0dac3ebaef
Chris Barrett <chris@codesaru.com>
2017-10-11 08:52:01 -0700

    Add switching between Python 2 and 3.

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
+PYTHON2_CFLAGS := -DPYTHON2 $(shell python-config --cflags)
+PYTHON3_CFLAGS := -DPYTHON3 $(shell python3-config --cflags)
+PYTHON2_LDFLAGS := $(shell python2-config --ldflags)
 PYTHON3_LDFLAGS := $(shell python3-config --ldflags)
-PYTHON3_CFLAGS := $(shell python3-config --cflags)
 
 CXX := clang++ --std=c++14
-CXX += $(PYTHON3_LDFLAGS) $(PYTHON3_CFLAGS)
+CXX += $(PYTHON2_LDFLAGS) $(PYTHON2_CFLAGS)
 
 a.out: main.cpp
 	$(CXX) main.cpp
--- main.cpp
+++ main.cpp
@@ -19,10 +19,14 @@ void runfile(const char * filename) {
 int main (int argc, const char ** argv) {
 	Py_Initialize();
 
+#ifdef PYTHON3
 	// Py_DecodeLocale doesn't exist until 3.5
 	//wchar_t * programName = Py_DecodeLocale(argv[0], nullptr);
 	wchar_t * programName = const_cast<wchar_t*>(L"a.out");
 	Py_SetProgramName(programName); // "optional but recommended"
+#else
+	Py_SetProgramName(const_cast<char*>(argv[0]));
+#endif
 
 	// test executing Python passed from program
 	PyRun_SimpleString("print('test {}'.format(2 + 8))");
@@ -30,7 +34,9 @@ int main (int argc, const char ** argv) {
 	runfile("test.py");
 
 	Py_Finalize();
-	//PyMem_RawFree(programName);
+#ifdef PYTHON3
+	PyMem_RawFree(programName);
+#endif
 	return 0;
 }
 
